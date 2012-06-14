22d50fb7ece34bc92b6cd1531965e2b0a3407451
hunt3ri <hunter_iain@yahoo.co.uk>
2012-06-14 10:30:19 +0100

    Working version of Nant.Builder with integrated unit testing

---
+++ src/Nant.Builder.Sample.Tests/RssServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nant.Builder.Sample.Services;
+
+namespace Nant.Builder.Sample.Tests
+{
+	[TestFixture]
+	public class RssServiceTests
+	{
+		[Test]
+		public void GetFeed_Should_Return_Some_Items()
+		{
+			// Arrange
+
+			var rssService = new RssService();
+
+			// Act
+
+			var feed = rssService.GetFeed("http://feeds.bbci.co.uk/news/rss.xml");
+
+			// Assert
+
+			Assert.That(feed.Channel.Items.Count(), Is.GreaterThan(0));
+
+		}
+	}
+}
